e77fb8dd704bc1c837d3945d60ab5057ef8c3770
Gabriel Albacarys <gabe@Gabriels-MBP.lan>
2014-12-12 20:32:45 -0600

    added JJ's files and removed Chris's old sequential code

--- src/sequential/motiondetect.cpp
+++ src/sequential/motiondetect.cpp
@@ -1,207 +1,296 @@
-// #include "opencv2/highgui/highgui.hpp"
-// #include "opencv2/opencv.hpp"
-// #include <iostream>
-//
-// using namespace cv;
-// using namespace std;
-//
-//
-// int detectMotion(const Mat & motion, Mat & result, Mat & result_cropped, int x_start, int x_stop, int y_start, int y_stop, int max_deviation, Scalar & color)
-// {
-//     Scalar mean, stddev;
-//     meanStdDev(motion, mean, stddev);
-//
-//     if(stddev[0] < max_deviation)
-//     {
-//       int num_changes = 0;
-//       int min_x = motion.cols, max_x = 0;
-//       int min_y = motion.rows, max_y = 0;
-//
-//       //Loop over image and detect changes
-//       for(int j = y_start; j < y_stop; j+=2){
-//           for(int i = x_start; i < x_stop; i+=2){
-//               if(static_cast<int>(motion.at<uchar>(j,i)) == 255)
-//               {
-//                   num_changes++;
-//                   if(min_x > i) min_x = i;
-//                   if(max_x < i) max_x = i;
-//                   if(min_y > j) min_y = j;
-//                   if(max_y < j) max_y = j;
-//               }
-//           }
-//       }
-//
-//       if(num_changes){
-//         if(min_x-10 > 0) min_x -= 10;
-//         if(min_y-10 > 0) min_y -= 10;
-//         if(max_x+10 < result.cols-1) max_x += 10;
-//         if(max_y+10 < result.rows-1) max_y += 10;
-//
-//         //Draw rectangle around the changed pixel
-//         Point x(min_x,min_y);
-//         Point y(max_x,max_y);
-//         Rect rect(x,y);
-//         Mat cropped = result(rect);
-//         cropped.copyTo(result_cropped);
-//         vector<vector<Point> > contours;
-//         findContours(motion,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-//         drawContours(result,contours,-1,Scalar(0,0,255),2);
-//         //rectangle(result,rect,color,1);
-//       }
-//
-//       return num_changes;
-//
-//     }
-//
-//     return 0;
-// }
-//
-// int main(int argc, char* argv[])
-// {
-//     VideoCapture cap(0); // open the video camera no. 0
-//     cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
-//     cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
-//
-//     if (!cap.isOpened())  // if not success, exit program
-//     {
-//         cout << "Cannot open the video cam" << endl;
-//         return -1;
-//     }
-//
-//     double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-//     double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-//
-//     cout << "Frame size : " << dWidth << " x " << dHeight << endl;
-//
-//     namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-//
-//     // Take images and convert them to gray
-//     Mat result, result_cropped, prev_frame, current_frame, next_frame;
-//     cap.read(prev_frame);
-//     result = prev_frame;
-//     cap.read(current_frame);
-//     cap.read(next_frame);
-//
-//     cvtColor(current_frame, current_frame, CV_RGB2GRAY);
-//     cvtColor(prev_frame, prev_frame, CV_RGB2GRAY);
-//     cvtColor(next_frame, next_frame, CV_RGB2GRAY);
-//
-//     /*Use d1 and d2 for calculating the differences
-//     result: result of and operation - used on d1 and d2
-//     num_changes: amount of changes in result matrix
-//     color: color for drawing rectangle when something changes TODO*/
-//
-//     Mat d1, d2, motion;
-//     int num_changes, num_sequence;
-//     Scalar mean_, color(0,255,255); //yellow
-//
-//     //Detect motion in window
-//     int x_start = 10, x_stop = current_frame.cols - 11;
-//     int y_start = 350, y_stop = 530;
-//
-//     //Motion threshold
-//     int motion_threshold = 5;
-//
-//     //Max deviation
-//     int max_deviation = 20;
-//
-//     //Rectangle TODO
-//     Mat kernel_ero = getStructuringElement(MORPH_RECT, Size(2,2));
-//
-//     //Now loop to detect motion
-//     while (1)
-//     {
-//         //Retreive new image
-//         prev_frame = current_frame;
-//         current_frame = next_frame;
-//         cap.read(next_frame);
-//         result = next_frame;
-//         cvtColor(next_frame, next_frame, CV_RGB2GRAY);
-//
-//         //Calculate difference between images and AND them
-//         //TODO replace this with cuda kernel
-//         absdiff(prev_frame, next_frame, d1);
-//         absdiff(next_frame, current_frame, d2);
-//         bitwise_and(d1, d2, motion);
-//         threshold(motion, motion, 35, 255, CV_THRESH_BINARY);
-//         erode(motion, motion, kernel_ero);
-//
-//         //Now detection Motion
-//         //TODO will be replaced by CUDA kernel
-//         num_changes = detectMotion(motion, result, result_cropped, x_start, x_stop, y_start, y_stop, max_deviation, color);
-//
-//         if(num_changes >= motion_threshold)
-//         {
-//             cout << "There is motion!!!" << endl;
-//             cout << "filler" << endl;
-//         }
-//
-//
-//         Mat frame;
-//
-//         bool bSuccess = cap.read(frame); // read a new frame from video
-//
-//         if (!bSuccess) //if not success, break loop
-//         {
-//             cout << "Cannot read a frame from video stream" << endl;
-//             break;
-//         }
-//
-//         imshow("MyVideo", result); //show the frame in "MyVideo" window
-//
-//         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-//         {
-//             cout << "esc key is pressed by user" << endl;
-//             break;
-//         }
-//
-//     }
-//
-//     return 0;
-//
-// }
+#include "png.h"
+#include "rgbapixel.h"
+#include <cmath>
+#include <ctime>
+#include <sys/ioctl.h>
 
-#include "opencv2/opencv.hpp"
-#include <iostream>
-#include <vector>
-#include "opencv2/highgui/highgui.hpp"
+#define GRAYSCALE 1
+#define GUASSIAN 1
+#define SOBEL 1
 
-using namespace cv;
+// custom settings
+#define INVERT 1
+#define GFILTER 1
+
+#define UPPERBOUND 100
+#define LOWERBOUND 30
+#define THRESHOLD 20
+
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-  Mat frame;
-  Mat back;
-  Mat fore;
-  Mat BGR_3[3];
-  VideoCapture cap(0);
-  BackgroundSubtractorMOG2 bg(3,false);
-
-  vector<vector<Point> > contours;
-
-  namedWindow("Frame");
-
-  while(1)
-    {
-      cap.read(frame);
-      split(frame,BGR_3);
-
-      /*for(int x = 0; x < 150; x++){
-        for(int y = 0; y < 150; y++){
-          frame.at<Vec3b>(y,x)[0] = 255;
-          frame.at<Vec3b>(y,x)[1] = 255;
-          frame.at<Vec3b>(y,x)[2] = 255;
-        }
-      }*/
-      bg.operator ()(frame,fore);
-      bg.getBackgroundImage(back);
-      erode(fore,fore,Mat());
-      dilate(fore,fore,Mat());
-      findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-      drawContours(frame,contours,-1,Scalar(0,0,255),2);
-      imshow("Frame",frame);
-      if(waitKey(30) >= 0) break;
+	clock_t runtime = clock();
+	// input png
+	string inputname("lumetta.png");
+	string outputname("lumettaout.png");
+   	PNG * image = new PNG;
+    if(!image->readFromFile(inputname)) {
+        cout << "\nError: unable to read image." << endl;
+        return -1;
     }
+    int imageWidth = image->width();
+    int imageHeight = image->height();
+
+    PNG *out = new PNG(imageWidth,imageHeight);
+	RGBAPixel *temp = new RGBAPixel;
+
+clock_t algorithmtime = clock();
+clock_t grayclock=0,gaussclock=0,sobelclock=0,edgeclock=0;
+
+if(GRAYSCALE){
+grayclock = clock();
+	cout<<"\nComputing grayscale filter..."<<flush;
+	//grayscale
+	for(int ix=0;ix<imageWidth;ix++){
+  		for(int iy=0;iy<imageHeight;iy++){
+  			int color =0;
+  			temp = (*image)(ix,iy);
+  			color+= temp->red;
+  			color+= temp->green;
+  			color+= temp->blue;
+  			color = color/3;
+  			temp->red  =color;
+  			temp->green=color;
+  			temp->blue =color;
+  			*(*out)(ix,iy) = *temp;
+  		}
+  	}
+grayclock = clock()-grayclock;
+  	cout<<"Done!"<<endl;
+}
+
+if(GUASSIAN){
+gaussclock = clock();
+	//int filterSum=159;int filter[5][5]={{2,4,5,4,2},{4,9,12,9,4},{5,12,15,12,5},{4,9,12,9,4},{2,4,5,4,2}};
+  	int filterSum=273;int filter[5][5]={{1,4,7,4,1},{4,16,26,16,4},{7,26,41,26,1},{4,16,26,16,4},{1,4,7,4,1}};
+
+  	cout<<"Computing Guassian convolution..."<<flush;
+  	for(int iy=2;iy<imageHeight-2;iy++){
+  		for(int ix=2;ix<imageWidth-2;ix++){
+  			int sumRed=0;
+  			int sumGreen=0;
+  			int sumBlue=0;
+  			//load the surrounding pixels into convenient 3x3 pixel block
+  			int irs[5][5] = {
+  							{(*image)(ix-2,iy-2)->red, (*image)(ix-1,iy-2)->red, (*image)(ix,iy-2)->red, (*image)(ix+1,iy-2)->red, (*image)(ix+2,iy-2)->red},
+  							{(*image)(ix-2,iy-1)->red, (*image)(ix-1,iy-1)->red, (*image)(ix,iy-1)->red, (*image)(ix+1,iy-1)->red, (*image)(ix+2,iy-1)->red},
+  							{(*image)(ix-2,iy)->red,   (*image)(ix-1,iy)->red,   (*image)(ix,iy)->red,   (*image)(ix+1,iy)->red,   (*image)(ix+2,iy)->red},
+  							{(*image)(ix-2,iy+1)->red, (*image)(ix-1,iy+1)->red, (*image)(ix,iy+1)->red, (*image)(ix+1,iy+1)->red, (*image)(ix+2,iy+1)->red},
+  							{(*image)(ix-2,iy+2)->red, (*image)(ix-1,iy+2)->red, (*image)(ix,iy+2)->red, (*image)(ix+1,iy+2)->red, (*image)(ix+2,iy+2)->red}
+  							};
+  			int igs[5][5] = {
+  							{(*image)(ix-2,iy-2)->green, (*image)(ix-1,iy-2)->green, (*image)(ix,iy-2)->green, (*image)(ix+1,iy-2)->green, (*image)(ix+2,iy-2)->green},
+  							{(*image)(ix-2,iy-1)->green, (*image)(ix-1,iy-1)->green, (*image)(ix,iy-1)->green, (*image)(ix+1,iy-1)->green, (*image)(ix+2,iy-1)->green},
+  							{(*image)(ix-2,iy)->green,   (*image)(ix-1,iy)->green,   (*image)(ix,iy)->green,   (*image)(ix+1,iy)->green,   (*image)(ix+2,iy)->green},
+  							{(*image)(ix-2,iy+1)->green, (*image)(ix-1,iy+1)->green, (*image)(ix,iy+1)->green, (*image)(ix+1,iy+1)->green, (*image)(ix+2,iy+1)->green},
+  							{(*image)(ix-2,iy+2)->green, (*image)(ix-1,iy+2)->green, (*image)(ix,iy+2)->green, (*image)(ix+1,iy+2)->green, (*image)(ix+2,iy+2)->green}
+  							};
+  			int ibs[5][5] = {
+  							{(*image)(ix-2,iy-2)->blue, (*image)(ix-1,iy-2)->blue, (*image)(ix,iy-2)->blue, (*image)(ix+1,iy-2)->blue, (*image)(ix+2,iy-2)->blue},
+  							{(*image)(ix-2,iy-1)->blue, (*image)(ix-1,iy-1)->blue, (*image)(ix,iy-1)->blue, (*image)(ix+1,iy-1)->blue, (*image)(ix+2,iy-1)->blue},
+  							{(*image)(ix-2,iy)->blue,   (*image)(ix-1,iy)->blue,   (*image)(ix,iy)->blue,   (*image)(ix+1,iy)->blue,   (*image)(ix+2,iy)->blue},
+  							{(*image)(ix-2,iy+1)->blue, (*image)(ix-1,iy+1)->blue, (*image)(ix,iy+1)->blue, (*image)(ix+1,iy+1)->blue, (*image)(ix+2,iy+1)->blue},
+  							{(*image)(ix-2,iy+2)->blue, (*image)(ix-1,iy+2)->blue, (*image)(ix,iy+2)->blue, (*image)(ix+1,iy+2)->blue, (*image)(ix+2,iy+2)->blue}
+  							};
+
+			//convolve preloaded pixels with filter
+			for(int sx=0;sx<5;sx++){
+				for(int sy=0;sy<5;sy++){
+					sumRed  +=irs[sy][sx]*filter[sy][sx];
+					sumGreen+=igs[sy][sx]*filter[sy][sx];
+					sumBlue +=ibs[sy][sx]*filter[sy][sx];
+				}
+			}
+			//divide by filterSum
+			temp->red = (sumRed/filterSum >255)?255:sumRed/filterSum;
+			temp->green = (sumGreen/filterSum >255)?255:sumGreen/filterSum;
+			temp->blue = (sumBlue/filterSum >255)?255:sumBlue/filterSum;
+
+			//set pixel
+			*(*out)(ix,iy) = *temp;
+  		}
+  	}
+gaussclock = clock()-gaussclock;
+  	cout<<"Done!"<<endl;
+}
+
+if(SOBEL){
+sobelclock = clock();
+  	int gx[3][3] = {{-1, 0, 1},
+					{-2, 0, 2},
+					{-1, 0, 1}};
+
+	int gy[3][3] = {{ 1, 2, 1},
+					{ 0, 0, 0},
+					{-1,-2,-1}};
+
+	//grad and edge dir
+	int* gradients = new int[imageHeight*imageWidth];
+	int* dirs = new int[imageHeight*imageWidth];
+
+	cout<<"Computing Sobel convolution..."<<flush;
+	//sobel convolution
+	//int iw=0;
+	//int ih=0;
+	//if(imageWidth<imageHeight){iw=imageHeight; ih=imageWidth;}else{iw=imageWidth; ih=imageHeight;}
+
+  	for(int iy=1;iy<imageHeight-1;iy++){
+  	for(int ix=1;ix<imageWidth-1;ix++){
+  		int sumx=0;
+  		int sumy=0;
+  		//load the surrounding pixels into convenient 3x3 pixel block
+  		int irs[3][3] = {{(*image)(ix-1,iy-1)->red, (*image)(ix,iy-1)->red, (*image)(ix+1,iy-1)->red},
+						 {(*image)(ix-1,iy  )->red, (*image)(ix,iy  )->red, (*image)(ix+1,iy  )->red},
+						 {(*image)(ix-1,iy+1)->red, (*image)(ix,iy+1)->red, (*image)(ix+1,iy+1)->red}};
+
+		//convolve preloaded pixels with gx,gy masks
+		for(int sx=0;sx<3;sx++){
+			for(int sy=0;sy<3;sy++){
+				sumx+=irs[sy][sx]*gx[sy][sx];
+				sumy+=irs[sy][sx]*gy[sy][sx];
+			}
+		}
+
+		//take absolute value
+		sumx=(sumx<0)?(sumx*-1):sumx;
+		sumy=(sumy<0)?(sumy*-1):sumy;
+		//add sumx+sumy, sumx is now total sum.
+		sumx += sumy;
+		//save your gradients!
+		if(sumx<THRESHOLD){sumx=0;}
+		gradients[iy*imageWidth+ix]=sumx;
+
+		//calculate the angles
+		float theta = atan2(sumy,sumx)/3.1415926 *180.0;
+		int thetatwo;
+		//Convert angle to approximate degree value
+		if( ((theta < 22.5) && (theta > -22.5)) || (theta > 157.5) || (theta < -157.5)   ){thetatwo=  0;}
+		if( ((theta > 22.5) && (theta <  67.5)) || ((theta <-112.5) && (theta > -157.5)) ){thetatwo= 45;}
+		if( ((theta > 67.5) && (theta < 112.5)) || ((theta < -67.5) && (theta > -112.5)) ){thetatwo= 90;}
+		if( ((theta >112.5) && (theta < 157.5)) || ((theta < -22.5) && (theta > -67.5))  ){thetatwo=135;}
+
+		//save your directions!
+		dirs[iy*imageWidth+ix]=thetatwo;
+
+		if(sumx>255){sumx=255;}
+		temp->red = 255-sumx;
+		temp->green = 255-sumx;
+		temp->blue = 255-sumx;
+		if(INVERT){
+			temp->red = sumx;
+			temp->green = sumx;
+			temp->blue = sumx;
+		}
+		*(*out)(ix,iy)=*temp;
+  	}}//EOL
+  	cout<<"Done!"<<endl;
+sobelclock = clock()-sobelclock;
+  	RGBAPixel *EDGE = new RGBAPixel(255,255,255,255);
+  	RGBAPixel *NONE = new RGBAPixel(  0,  0,  0,255);
+
+  	if(INVERT){
+  		NONE->red=255;
+  		NONE->green=255;
+  		NONE->blue=255;
+  		EDGE->red=0;
+  		EDGE->green=0;
+  		EDGE->blue=0;
+  	}
+
+  	cout<<"Computing edges..."<<flush;
+edgeclock = clock();
+
+  	for(int iy=1;iy<imageHeight-1;iy++){
+    for(int ix=1;ix<imageWidth-1;ix++){
+
+    	//gradient should be greater than UPPERBOUND
+      if(gradients[iy*imageWidth+ix] > UPPERBOUND){
+      	//currDir is the direction: 0,45,90,135
+        int currDir = dirs[iy*imageWidth+ix];
+        bool edgeFinished = false;
+        int nextx=0,nexty=0;
+        switch(currDir){
+          case 0:
+            nextx = ix+1;// check x in next direction
+            nexty = iy;
+            //Find and color next pixel in the same direction, within the bounds defined above
+            while( !edgeFinished && (dirs[nexty*imageWidth+nextx]==currDir) && (gradients[nexty*imageWidth+nextx]>LOWERBOUND) ){
+              *(*out)(ix,iy)=*EDGE;
+              if(nextx>imageWidth-1){edgeFinished=true;}else{nextx+=1;} //increment x
+            }
+            break;
+          case  45:
+            nextx = ix+1;// check x in next direction
+            nexty = iy+1;// check y in next direction
+            while( !edgeFinished && (dirs[nexty*imageWidth+nextx]==currDir) && (gradients[nexty*imageWidth+nextx]>LOWERBOUND) ){
+              *(*out)(ix,iy)=*EDGE;
+              if(nextx>imageWidth-1){edgeFinished=true;}else{nextx+=1;}
+              if(nexty>imageHeight-1){edgeFinished=true;}else{nexty+=1;}
+            }
+            break;
+          case  90:
+            nextx = ix;
+            nexty = iy+1;// check y in next direction
+            while( !edgeFinished && (dirs[nexty*imageWidth+nextx]==currDir) && (gradients[nexty*imageWidth+nextx]>LOWERBOUND) ){
+              *(*out)(ix,iy)=*EDGE;
+              if(nexty>imageHeight-1){edgeFinished=true;}else{nexty+=1;} //increment y
+            }
+            break;
+          case 135:
+            nextx = ix+1;//
+            nexty = iy-1;// check y in next direction
+            while( !edgeFinished && (dirs[nexty*imageWidth+nextx]==currDir) && (gradients[nexty*imageWidth+nextx]>LOWERBOUND) ){
+              *(*out)(ix,iy)=*EDGE;
+              if(nextx>imageWidth-1){edgeFinished=true;}else{nextx+=1;}
+              if(nexty>imageHeight-1 || nexty < 0){edgeFinished=true;}else{nexty-=1;}
+            }
+            break;
+          default : *(*out)(ix,iy)=*NONE;
+        }
+      }else{
+        *(*out)(ix,iy)=*NONE;
+      }
+    }}//EOL
+
+  	//cleanup non binary colors
+  	for(int iy=1;iy<imageHeight-1;iy++){
+  	for(int ix=1;ix<imageWidth-1;ix++){
+  		if(*(*out)(ix,iy) != *EDGE ){
+  			*(*out)(ix,iy)=*NONE;
+  		}
+  	}}//EOL
+edgeclock = clock()-edgeclock;
+algorithmtime = clock()-algorithmtime;
+
+  	cout<<"Done!"<<endl;
+
+  	delete EDGE;
+  	delete NONE;
+  	delete gradients;
+  	delete dirs;
+}
+
+  	out->writeToFile(outputname);
+  	delete image;
+  	delete out;
+
+  	runtime = clock()-runtime;
+
+  	struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+
+  	cout<<"\nPerformance Results for: "<<imageWidth<<","<<imageHeight<<" px"<<endl;
+  	for(int minux=0;minux<w.ws_col;minux++){
+  		cout<<"-";
+  	}
+  	cout<<endl;
+  	cout<<"Overall  : "<<runtime<<" clicks ("<<(float)runtime/CLOCKS_PER_SEC<<"s)"<<endl;
+  	cout<<"Algorithm: "<<algorithmtime<<" clicks ("<<(float)algorithmtime/CLOCKS_PER_SEC<<"s)"<<endl;
+  	cout<<"Grayscale: "<<grayclock<<" clicks ("<<(float)grayclock/CLOCKS_PER_SEC<<"s)"<<endl;
+  	cout<<"Gaussian : "<<gaussclock<<" clicks ("<<(float)gaussclock/CLOCKS_PER_SEC<<"s)"<<endl;
+  	cout<<"Sobel    : "<<sobelclock<<" clicks ("<<(float)sobelclock/CLOCKS_PER_SEC<<"s)"<<endl;
+  	cout<<"Edges    : "<<edgeclock<<" clicks ("<<(float)edgeclock/CLOCKS_PER_SEC<<"s)"<<endl;
+  	cout<<endl;
     return 0;
-  }
+}
--- src/sequential/sequential.cpp
+++
@@ -1,5 +0,0 @@
-#import "../easypng/png.h"
-
-void sequential_algorithm(PNG * image) {
-
-}
